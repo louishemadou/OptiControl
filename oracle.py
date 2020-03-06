552de0a8f2b14b9ca971ae9aae44ac1d4c85a8c8
RomainBertrand <romaincmoi@laposte.net>
2020-03-06 11:16:34 +0100

    fonction oracle

--- oracle.py
+++ oracle.py
@@ -3,10 +3,13 @@ import numpy as np
 from .Probleme_R import *
 from .Structures_N import *
 
-def OraclePG(qc, ind):
-    if ind == 2:
-        return (1/3)*np.dot(q0 + np.dot(B, qc), r*(q0 + np.dot(B, qc))*np.abs(q0 + np.dot(B, qc))) + np.dot(pr, np.dot(Ar, q0 + np.dot(B, qc)))
-    if ind == 3:
-        q = q0 + np.dot(B, qc)
-        return np.dot(B.T, r*q*np.abs(q)) + np.dot(np.dot(B.T, Ar.T), pr)
 
+def OraclePG(qc, ind):
+    q = q0 + np.dot(B, qc)
+    F = 0
+    G = 0
+    if ind == 2 or ind == 4:
+        F = (1/3)*np.dot(q, r*q*np.abs(q)) + np.dot(pr, np.dot(Ar, q))
+    if ind == 3 or ind == 4:
+        G = np.dot(B.T, r*q*np.abs(q)) + np.dot(np.dot(B.T, Ar.T), pr)
+    return [F, G, ind]
