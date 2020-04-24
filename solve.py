1e5a9cabb8490e9f3e1674c844aa5cd832c58a98
Louis Hémadou <louis.hemadou@eleves.enpc.fr>
2020-04-24 22:12:59 +0200

    fixed solve

--- solve.py
+++ solve.py
@@ -6,29 +6,23 @@ from Gradient_F import Gradient_F
 
 from time import process_time
 
-def solve(method, visual=False):
+def solve(method, oracle, visual=False):
     x0 = np.random.normal(size=n - md)
     time_start = process_time()
     if method == "BFGS":
-        F, x, G = bfgs(OraclePG, x0, visual=visual)
+        F, x, G = bfgs(oracle, x0, visual=visual)
     if method == "Newton":
-        F, x, G = Newton(OraclePH, x0, visual=visual)
+        F, x, G = Newton(oracle, x0, visual=visual)
     if method == "PR":
-        F, x, G = Polak_Ribiere(OraclePG, x0, visual=visual)
+        F, x, G = Polak_Ribiere(oracle, x0, visual=visual)
     if method == "Gradient_F":
-        F, x, G = Gradient_F(OraclePG, x0, visual = visual)
-    if method == "Dual":
-        u0 = np.random.normal(size=md)
-        F, x, G = Newton(OracleDH, u0, visual=visual)
-        F, x, G = Gradient_F(OracleDG, u0, visual=visual)
-        F, x, G = bfgs(OracleDG, u0, visual = visual)
-        F, x, G = Polak_Ribiere(OracleDG, u0, visual=visual)
+        F, x, G = Gradient_F(oracle, x0, visual = visual)
     cpu_time = process_time() - time_start
     print("Temps d'exécution : ", cpu_time)
     return F, x, G
 
 
 if __name__ == '__main__':
-    F, x, G = solve("Dual", visual=True)
+    F, x, G = solve("BFGS", OracleDG, visual=True)
     print("Critère optimal = " + str(F))
     print("Optimal au point " + str(x))
